b3d06b65609cea2bb99c3d344068a9c0db5c9039
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-06 20:55:33 -0300

    for in ejs

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-let user = {
+let users1 = [
+    {
     name: "Davi Silva Sauro",
     phone: "2444-66666"
+},
+{
+    name: "Maria",
+    phone: "2444-66666"
+},
+{
+    name: "Doly",
+    phone: "2444-66666"
 }
+
+]
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("views engine", "ejs");
 
 app.get("/", (req, res)=>{
 
-    res.render('user.ejs', user)
-    // res.render('user.ejs', {name: "Davi Silva Santos", phone: "(44) 4444-44444"})
-
-
+    res.render('user.ejs', {users: users1})
 })
 
 app.listen(3000, () =>{
