d1da2d71d5ee54e2ff4950514d5dd4676b985f58
Davi Silva Santos <morg.daviss@gmail.com>
2022-04-06 17:38:07 -0300

    substituicao de variave

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-// const port = 3000;
+const port = 3000;
+
+let user = {
+    name: "Davi Silva Sauro",
+    phone: "2444-66666"
+}
 
 app.set("views", path.join(__dirname, 'views'));
 app.set("views engine", "ejs");
 
 app.get("/", (req, res)=>{
 
-    res.render('user.ejs')
+    res.render('user.ejs', user)
 
 })
 
 app.listen(3000, () =>{
-    console.log(`server running...`)
+    console.log(`server running on port ${port}...`)
 })
